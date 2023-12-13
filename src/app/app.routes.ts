2755aafdc97d9a30dd12a73b4d4cef642a4c4f53
Lucas Estevão De Oliveira <luccaseoliveira@gmail.com>
2023-12-13 12:59:55 -0300

    Defines project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DeckListComponent} from "@features/decks/deck-list/deck-list.component";
+import {DeckCreateComponent} from "@features/decks/deck-create/deck-create.component";
+import {DeckDetailsComponent} from "@features/decks/deck-details/deck-details.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'deck-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'deck-list',
+    component: DeckListComponent
+  },
+  {
+    path: 'deck-create',
+    component: DeckCreateComponent,
+  },
+  {
+    path: 'deck-details',
+    component: DeckDetailsComponent
+  }
+];
---
+++ src/app/features/decks/deck-details/deck-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deck-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './deck-details.component.html',
+  styleUrl: './deck-details.component.scss'
+})
+export class DeckDetailsComponent {
+
+}
