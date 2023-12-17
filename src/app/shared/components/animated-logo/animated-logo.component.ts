e7cc617f1de5cffe499a52f0c9e4f9cdad0f1130
Lucas Estevão De Oliveira <luccaseoliveira@gmail.com>
2023-12-17 11:34:43 -0300

    Adds a logo for the header

---
+++ src/app/shared/components/animated-logo/animated-logo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-animated-logo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './animated-logo.component.html',
+  styleUrl: './animated-logo.component.scss'
+})
+export class AnimatedLogoComponent {
+
+}
